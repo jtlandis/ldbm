d957de3d49992bc8681b70bc215f394f4f0d1136
jtlandis <30324172+jtlandis@users.noreply.github.com>
2020-06-18 20:19:25 -0400

    some documentation

--- R/Suplimental.R
+++ R/Suplimental.R
@@ -1,20 +1,31 @@
 
+#' @name gen_random
+#' @title Generate Random string
+#' @description Function used to generate random strings
+#' for databases when no name is provided.
 gen_random <- function(n = 6){
   paste(sample(c(letters,0:9), size = n, T,prob = c(rep(.5/26,26),rep(.05,10))), collapse = "")
 }
 
 
-all_within <- function(x,y) {
-  if(length(unique(x))!=length(unique(y))){
-    return(FALSE)
-  }
-  if(length(intersect(x,y))!=length(unique(x))){
-    return(FALSE)
-  } else {
-    return(TRUE)
-  }
+#' @name all_within
+#' @title All Within
+#' @description wrapper to test all unique values of x and y are the same
+all_within <- function(x,y){
+  x <- unique(x)
+  y <- unique(y)
+  res <- all(x%in%y)&&all(y%in%x)
+  return(res)
 }
 
+#' @name invertnames
+#' @title Invert names
+#' @description Inverts the values and names of a named vector.
+#' @example 
+#' \dontrun{
+#' vec <- c("A"="a","B"="b")
+#' invertnames(vec)
+#' }
 invertnames <- function(x){
   if(is.null(names(x))){
     stop("x must be a named vector")
@@ -28,13 +39,27 @@ df_match <- function(x, y) {
   if(!all_within(colnames(x),colnames(y))){
     stop("x and y must have exact col names")
   }
-  y <- y[,colnames(x)]
+  y <- y[,colnames(x)] #reorders columns
   matches <- mapply(function(x,y){x%in%y}, x = x, y = y)
   matches <- apply(matches,1,all)
   matches
 }
 
+#' @name update_left
+#' @title Update Left
+#' @description Special function intended only to be
+#' used on two different version of the same data frame. 
+#' Intended to be used to efficently update entries into
+#' the master ldb if y is a subset. Only pulls in rows of
+#' y whose keys match the keys of x.
+#' @example 
+#' x <- data.frame(key1 = 1:4, key2 = letters[1:4], value1 = rnorm(4), value2 = runif(4))
+#' y <- x[c(2,4),]
+#' y$value1 <- -10
+#' update_left(x,y, by = c("key1"="key1","key2"="key2")) 
+#' @export
 update_left <- function(x, y, by = NULL){
+  #browser()
   if(!all_within(colnames(x),colnames(y))){
     stop("x and y must have the same colnames")
   }
@@ -45,13 +70,16 @@ update_left <- function(x, y, by = NULL){
     }
     key <- by
   } else {
-    colnames(y)[colnames(y)%in%by] <- names(by)
+    #colnames(y)[colnames(y)%in%by] <- names(by)
     key <- names(by)
   }
   .left <- x %>% select(key)
   .right <- y %>% select(key)
   
   matched <- df_match(x = .left, y = .right)
+  if(sum(matched)!=nrow(y)){
+    stop("Cannot match by specified keys distinctly.")
+  }
   index <- apply(.left[matched,],
                  1,
                  function(x,y){which(apply(y,
